1755f5b7a00a22ab92ec090d564c3706881a5d2e
m87655 <55180804+m87655@users.noreply.github.com>
2020-01-06 13:22:00 +0800

    攝影機

---
+++ Unity_106051070_WU/Assets/Scenes/CameraControl.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public Transform cam,target;
+    public float speed;
+
+    private void Update()
+    {
+        Vector3 pos = Vector3.Lerp(cam.position, target.position, 0.5f * speed * Time.deltaTime);
+        cam.position = pos;
+    }
+}
+
